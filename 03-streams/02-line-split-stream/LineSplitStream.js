5ec79d0601cd7a13fb966849ad7a73a871a1dee9
Artem Marakutsa <artmar14@gmail.com>
2021-05-21 18:37:24 +0300

    solution for Line Split stream

--- 03-streams/02-line-split-stream/LineSplitStream.js
+++ 03-streams/02-line-split-stream/LineSplitStream.js
@@ -33,21 +33,4 @@ class LineSplitStream extends stream.Transform {
   }
 }
 
-const lines = new LineSplitStream({
-  encoding: 'utf-8',
-});
-
-function onData(line) {
-  console.log(line);
-}
-
-lines.on('data', onData);
-
-lines.write(`первая строка${os.EOL}вторая строка${os.EOL}третья строка`);
-
-lines.end();
-
-
-
-
 module.exports = LineSplitStream;
