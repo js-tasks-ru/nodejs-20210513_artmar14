eb0fa446614c8bf7a86514f63673e21a2a22450a
Artem <64262318+artm4r@users.noreply.github.com>
2021-06-04 13:04:03 +0300

    solution for 02-rest-api

--- 07-mongodb-mongoose/02-rest-api/controllers/products.js
+++ 07-mongodb-mongoose/02-rest-api/controllers/products.js
@@ -1,12 +1,52 @@
+const mongoose = require('mongoose');
+const Product = require('../models/Product');
+
+const formatProduct = (product) => ({
+  id: product._id,
+  title: product.title,
+  images: product.images,
+  category: product.category._id,
+  subcategory: product.subcategory._id,
+  price: product.price,
+  description: product.description
+});
+
+const formatProductList = (products) => ({
+  products: products.map(formatProduct)
+});
+
 module.exports.productsBySubcategory = async function productsBySubcategory(ctx, next) {
-  ctx.body = {};
+  const subcategory = ctx.request?.query?.subcategory;
+  if (subcategory) {
+    ctx.status = 200;
+    ctx.body = formatProductList(await Product.find({ subcategory }))
+  } else {
+    await next();
+  }
 };
 
 module.exports.productList = async function productList(ctx, next) {
-  ctx.body = {};
+  ctx.status = 200;
+  ctx.body = formatProductList(await Product.find({}))
 };
 
 module.exports.productById = async function productById(ctx, next) {
-  ctx.body = {};
+
+  const productId = ctx.params?.id;
+
+  if (productId) {
+    if (mongoose.Types.ObjectId.isValid(productId)) {
+      const product = await Product.findById(productId)
+      if (product) {
+        ctx.body = {
+          product: formatProduct(product)
+        };
+      } else {
+        ctx.status = 404
+      }
+    } else {
+      ctx.status = 400
+    }
+  }
 };
 
