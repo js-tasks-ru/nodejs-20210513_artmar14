09d3d636c2cb89c54f004847b428a5a47cec0681
Artem Marakutsa <artmar14@gmail.com>
2021-06-06 09:10:11 +0300

    03-search solution

--- 07-mongodb-mongoose/03-search/controllers/products.js
+++ 07-mongodb-mongoose/03-search/controllers/products.js
@@ -1,3 +1,26 @@
+const Product = require('../models/Product');
+
+const formatProduct = (product) => ({
+  id: product._id,
+  title: product.title,
+  images: product.images,
+  category: product.category,
+  subcategory: product.subcategory,
+  price: product.price,
+  description: product.description,
+});
+
 module.exports.productsByQuery = async function productsByQuery(ctx, next) {
-  ctx.body = {products: []};
+  const query = ctx.request?.query?.query;
+  if (query) {
+    const products = await Product
+        .find(
+            { $text: { $search: query } },
+            { score: { $meta: "textScore" } }
+        )
+        .sort({ score: { $meta: 'textScore' } })
+
+    ctx.status = 200;
+    ctx.body = { products: products.map(formatProduct) };
+  }
 };
