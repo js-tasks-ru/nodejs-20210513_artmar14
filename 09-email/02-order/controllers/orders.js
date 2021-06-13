f2bef25c9ef196b9d1a87c35c8c75557b5a71128
Artem Marakutsa <artmar14@gmail.com>
2021-06-13 15:39:08 +0300

    02-order solution

--- 09-email/02-order/controllers/orders.js
+++ 09-email/02-order/controllers/orders.js
@@ -2,7 +2,48 @@ const Order = require('../models/Order');
 const sendMail = require('../libs/sendMail');
 
 module.exports.checkout = async function checkout(ctx, next) {
+
+  const { product, phone, address } = ctx?.request?.body ?? {};
+
+  let order = await Order.create({ product, phone, address, user: ctx.user._id });
+  order = await order.populate('product').execPopulate()
+
+  await sendMail({
+    template: 'order-confirmation',
+    locals: {
+      id: order._id,
+      product: {
+        title: order.product.title
+      }
+    },
+    to: 'user@mail.com',
+    subject: 'Подтвердите почту',
+  });
+
+  ctx.statusCode = 200;
+  ctx.body = { order: order._id }
 };
 
 module.exports.getOrdersList = async function ordersList(ctx, next) {
+
+  const orders = await Order.find({ user: ctx.user._id }).populate('product');
+  ctx.statusCode = 200;
+
+  ctx.body = {
+    orders: orders.map(({ _id: id, user, phone, address, product }) => ({
+      id,
+      user,
+      phone,
+      address,
+      product: {
+        id: product._id,
+        title: product.title,
+        images: product.images,
+        category: product.category,
+        subcategory: product.subcategory,
+        price: product.price,
+        description: product.description,
+      },
+    }))
+  }
 };
